5e9461e94a028f9881b08c6f13536a2fbe04032a
virajsaranga <virajridi@gmail.com>
2025-03-09 08:35:51 +0530

    app.js updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,12 @@ import Header from './component/Header'
 import HeroSection from './component/HeroSection'
 import FeaturedSection from './component/FeaturedSection'
 import ThanksPage from './component/ThanksPage'
+import TravelGuides from './component/TravelGuides'
+import DestinationsSection from './component/DestinationsSection'
+import RecentPosts from './component/RecentPosts'
+import GetInTouch from './component/GetInTouch'
+import Newsletter from './component/Newsletter'
+import Footer from './component/Footer'
 
 
 
@@ -16,6 +22,12 @@ function App() {
       <HeroSection />
       <FeaturedSection />
       <ThanksPage />
+      <TravelGuides />
+      <DestinationsSection />
+      <RecentPosts />
+      <GetInTouch />
+      <Newsletter />
+      <Footer />
     </>
   )
 }
