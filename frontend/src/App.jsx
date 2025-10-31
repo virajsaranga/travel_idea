c84c107d48952a9f7f3b0d7e9820b6ad448023a8
virajsaranga <virajridi@gmail.com>
2025-10-31 12:48:05 +0530

    route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,8 @@ import TravelGuides from './component/TravelGuides'
 import DestinationsSection from './component/DestinationsSection'
 import RecentPosts from './component/RecentPosts'
 import GetInTouch from './component/GetInTouch'
-import Newsletter from './component/Newsletter'
 import Footer from './component/Footer'
+import FaqChecklist from "./component/FaqChecklist";
 
 
 
@@ -26,6 +26,9 @@ function App() {
       <div id="DestinationsSection"><DestinationsSection /></div>
       <div id="RecentPosts"><RecentPosts /></div>
       <div id="contact"><GetInTouch /></div>
+      <div id="faq"><FaqChecklist /></div>
+
+
     
       <div id="about"><Footer /></div>
     </>
